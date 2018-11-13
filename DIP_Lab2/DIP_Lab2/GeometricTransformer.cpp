c35424c603bcee96e54fc2f4cde069dd8e3cd411
NhatHuyTran <nhathuy13598@gmail.com>
2018-11-13 22:13:59 +0700

    Hoan thanh Lab 2

--- DIP_Lab2/DIP_Lab2/GeometricTransformer.cpp
+++ DIP_Lab2/DIP_Lab2/GeometricTransformer.cpp
@@ -37,6 +37,7 @@ void NearestNeighborInterpolate::Interpolate(
 	float tx, float ty,
 	uchar* pSrc, int srcWidthStep, int nChannels,
 	uchar* pDstRow) {
+
 	int nb[4][2] = { {floor(tx), floor(ty)}, // tọa độ của 4 điểm lân cận gần nhất của điểm (tx, ty)
 					 {ceil(tx), floor(ty)},
 					 {ceil(tx), ceil(ty)},
@@ -67,7 +68,7 @@ void NearestNeighborInterpolate::Interpolate(
 }
 
 void AffineTransform::Translate(float dx, float dy) {
-	float data[9] = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, dx, dy, 1.0 };
+	float data[9] = { 1,0,dx,0,1,dy,0,0,1 };
 	_matrixTransform = Mat(3, 3, CV_32F);
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -76,14 +77,13 @@ void AffineTransform::Translate(float dx, float dy) {
 	}
 }
 void AffineTransform::Rotate(float angle) {
-	float data[9] = { cos(angle), sin(angle), 0.0, -1*sin(angle), cos(angle), 0.0, 0.0, 0.0, 1.0 };
+	float data[9] = { cos(angle), -sin(angle), 0, sin(angle), cos(angle), 0.0, 0.0, 0.0, 1.0 };
 	_matrixTransform = Mat(3, 3, CV_32F);
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			_matrixTransform.at<float>(i, j) = data[i*3+j];
 		}
 	}
-	cout << _matrixTransform << endl;
 }
 void AffineTransform::Scale(float sx, float sy) {
 	float data[9] = { sx, 0.0, 0.0, 0.0, sy, 0.0, 0.0, 0.0, 1.0};
@@ -95,10 +95,10 @@ void AffineTransform::Scale(float sx, float sy) {
 	}
 }
 void AffineTransform::TransformPoint(float &x, float &y) {
-	Mat matrixInverse = _matrixTransform.inv();// ma trận nghịch đảo của matrixInverse
+	
 	float v, w;
-	v = x * matrixInverse.at<float>(0, 0) + y * matrixInverse.at<float>(1, 0) + matrixInverse.at<float>(2, 0);
-	w = x * matrixInverse.at<float>(0, 1) + y * matrixInverse.at<float>(1, 1) + matrixInverse.at<float>(2, 1);
+	v = x * _matrixTransform.at<float>(0, 0) + y * _matrixTransform.at<float>(0,1) + _matrixTransform.at<float>(0,2);
+	w = x * _matrixTransform.at<float>(1,0) + y * _matrixTransform.at<float>(1, 1) + _matrixTransform.at<float>(1, 2);
 	x = v;
 	y = w;
 }
@@ -108,6 +108,7 @@ int GeometricTransformer::Transform(
 	Mat &afterImage,
 	AffineTransform* transformer,
 	PixelInterpolate* interpolator) {
+
 	uchar* pData1 = (uchar*)beforeImage.data;
 	uchar* pData2 = (uchar*)afterImage.data;
 	int height = afterImage.rows, width = afterImage.cols;
@@ -120,50 +121,143 @@ int GeometricTransformer::Transform(
 		uchar* pPoint;
 		for (int x2 = 0; x2 < width; x2++, pRow2 += nChannels) {
 			float x1 = x2, y1 = y2;
-			transformer->TransformPoint(x1, y1);
+			
+			
+			transformer->TransformPoint(x1,y1); 
 			pPoint = pData1;
+			
 			if (int(x1) == x1 && int(y1) == y1) {//backward mapping cho diem trung voi diem trong input
+				
 				pPoint += int(y1) * widthStep1 + int(x1) * nChannels;
 				pRow2[0] = pPoint[0];
 				pRow2[1] = pPoint[1];
 				pRow2[2] = pPoint[2];
 			}
 			else {
-				interpolator->Interpolate(x1, y1, pPoint, widthStep1, nChannels, pRow2);
+				interpolator->Interpolate(x1, y1, pPoint, widthStep1, nChannels, pRow2); 
 			}
 		}
 	}
 	return 1;
 }
+
 int GeometricTransformer::RotateKeepImage(
 	const Mat &srcImage, Mat &dstImage, float angle, PixelInterpolate* interpolator) {
+
+	// Tạo khung hình mới
+	int diagonal = (int)sqrt(srcImage.cols*srcImage.cols + srcImage.rows*srcImage.rows);
+	int newWidth = diagonal;
+	int newHeight = diagonal;
+
+
+	dstImage = Mat(newHeight, newWidth, CV_8UC3, Scalar(0, 0, 0));
+	AffineTransform* transformer = new AffineTransform;
+	transformer->Rotate(angle);
+
+	int offsetX = (newWidth - srcImage.cols) / 2;
+	int offsetY = (newHeight - srcImage.rows) / 2;
+
+	dstImage = Mat(newWidth, newHeight, srcImage.type());
+
+	Mat frame = Mat(newWidth, newHeight, srcImage.type());
+
+	// Hiệu chỉnh hình ảnh về giữa
+	srcImage.copyTo(frame.colRange(offsetX, offsetX + srcImage.cols).rowRange(offsetY, offsetY + srcImage.rows));
+
+
+	uchar* pData1 = (uchar*)frame.data;
+	uchar* pData2 = (uchar*)dstImage.data;
+	int nchannels = srcImage.channels();
+	int widthStep1 = frame.step[0];
+	int widthStep2 = dstImage.step[0];
+
+	// Lấy dòng và cột ảnh dstImage
+	int rows = dstImage.rows;
+	int cols = dstImage.cols;
+
+	// Lấy nửa dòng và cột ảnh dstImage
+	int rowHalf = rows / 2;
+	int colHalf = cols / 2;
+	for (int y2 = -rowHalf; y2 < rowHalf; y2++, pData2 += widthStep2)
+	{
+		uchar* pRow2 = pData2;
+		uchar* pPoint;
+		float x1, y1;
+		for (int x2 = -colHalf; x2 < colHalf; x2++, pRow2 += nchannels)
+		{
+			x1 = x2;
+			y1 = y2;
+			transformer->TransformPoint(x1, y1);
+			x1 += colHalf;
+			y1 += rowHalf;
+			if (x1 < 0 || x1 > cols || y1 < 0 || y1 > rows)
+				continue;
+			pPoint = pData1;
+			if (int(x1) == x1 && int(y1) == y1)
+			{
+				pPoint += int(y1) *widthStep1 + int(x1) *nchannels;
+				pRow2[0] = pPoint[0];
+				pRow2[1] = pPoint[1];
+				pRow2[2] = pPoint[2];
+			}
+			else
+				interpolator->Interpolate(x1, y1, pPoint, widthStep1, nchannels, pRow2);
+		}
+	}	
+	delete transformer;
 	return 1;
 }
 int GeometricTransformer::RotateUnkeepImage(
 	const Mat &srcImage, Mat &dstImage, float angle, PixelInterpolate* interpolator) {
-	dstImage = Mat(srcImage.rows, srcImage.cols, CV_8UC3);
-	uchar* pSrc = srcImage.data;
-	uchar* pDst = dstImage.data;
-	int rows = srcImage.rows;
-	int cols = srcImage.cols;
-	int channels = srcImage.channels();
-	int widthStep = srcImage.step[0];
-	// Tạo ma trận Transform
-	AffineTransform *at = new AffineTransform();
-	at->Rotate(angle);
-	uchar* pRowDst = pDst;
-	for (int i = 0; i < rows; i++ , pRowDst += widthStep) {		
-		for (int j = 0; j < cols; j++ , pDst += channels) {
-			float iTemp = i;
-			float jTemp = j;
-			// Tìm điểm ảnh trên ảnh gốc dựa vào ảnh kết quả * ma trận nghịch đảo Transform
-			at->TransformPoint(iTemp,jTemp);
 
-			// Nội suy giá trị màu
-			interpolator->Interpolate(iTemp, jTemp, pSrc, widthStep, channels, pDst);
+	dstImage = Mat(srcImage.rows, srcImage.cols, CV_8UC3, Scalar(0, 0, 0));
+	AffineTransform* transformer = new AffineTransform;
+	transformer->Rotate(angle);
+
+	Mat frame = srcImage;
+
+	uchar* pData1 = (uchar*)frame.data;
+	uchar* pData2 = (uchar*)dstImage.data;
+	int nchannels = srcImage.channels();
+	int widthStep1 = frame.step[0];
+	int widthStep2 = dstImage.step[0];
+
+	// Lấy dòng và cột ảnh dstImage
+	int rows = dstImage.rows;
+	int cols = dstImage.cols;
+
+	// Lấy nửa dòng và cột ảnh dstImage
+	int rowHalf = rows / 2;
+	int colHalf = cols / 2;
+
+	for (int y2 = -rowHalf; y2 < rowHalf; y2++, pData2 += widthStep2)
+	{
+		uchar* pRow2 = pData2;
+		uchar* pPoint;
+		float x1, y1;
+		for (int x2 = -colHalf; x2 < colHalf; x2++, pRow2 += nchannels)
+		{
+			x1 = x2;
+			y1 = y2;
+			transformer->TransformPoint(x1, y1);
+			x1 += colHalf;
+			y1 += rowHalf;
+			if (x1 < 0 || x1 > cols || y1 < 0 || y1 > rows)
+				continue;
+			pPoint = pData1;
+			if (int(x1) == x1 && int(y1) == y1)
+			{
+				pPoint += int(y1) *widthStep1 + int(x1) *nchannels;
+				pRow2[0] = pPoint[0];
+				pRow2[1] = pPoint[1];
+				pRow2[2] = pPoint[2];
+			}
+			else
+				interpolator->Interpolate(x1, y1, pPoint, widthStep1, nchannels, pRow2);
 		}
 	}
-	delete at;
+
+	delete transformer;
 	return 1;
 }
 int GeometricTransformer::Scale(
@@ -171,11 +265,14 @@ int GeometricTransformer::Scale(
 	Mat &dstImage,
 	float sx, float sy,
 	PixelInterpolate* interpolator) {
+
+
 	dstImage = Mat(srcImage.rows*sy, srcImage.cols*sx, CV_8UC3, Scalar(0, 0, 0));
 	AffineTransform *transformer;
 	transformer = new AffineTransform;
-	transformer->Scale(sx, sy);
+	transformer->Scale(1 / sx, 1 / sy);
 	Transform(srcImage, dstImage, transformer, interpolator);
+	delete transformer;
 	return 1;
 }
 
--- DIP_Lab2/DIP_Lab2/Source.cpp
+++ DIP_Lab2/DIP_Lab2/Source.cpp
@@ -1,74 +1,63 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <cmath>
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "GeometricTransformer.h"
-#define Pi 3.14
 
 using namespace std;
 using namespace cv;
-//int main(int argc, char *argv[])
-//{
-//	Mat inputImage = imread(argv[3], CV_LOAD_IMAGE_COLOR);
-//	Mat outputImage;
-//
-//	/*PixelInterpolate *interpolator;
-//	if(strcmp(argv[2],"--nn") == 0)
-//		interpolator = new NearestNeighborInterpolate;
-//	else if(strcmp(argv[2],"--bl") == 0)
-//		interpolator = new BilinearInterpolate;
-//	else {
-//		cout << strcmp(argv[2],"--bl")  << endl;
-//		cout << "Interpolation error " << endl;
-//		return 0;
-//	}
-//
-//	GeometricTransformer gt;
-//	if (strcmp(argv[1],"--zoom") == 0) {
-//		gt.Scale(inputImage, outputImage, atof(argv[4]), atof(argv[4]), interpolator);
-//	}
-//	else if (strcmp(argv[1],"--rotate") == 0) {
-//
-//	}
-//	else if (strcmp(argv[1],"--rotateN") == 0) {
-//
-//	} 
-//	else{
-//		cout << "Command error" << endl;
-//		return 0;
-//	}*/
-//
-//	GeometricTransformer gt;
-//	PixelInterpolate *interpolator;
-//	interpolator = new NearestNeighborInterpolate;
-//	gt.Scale(inputImage, outputImage, 2, 2, interpolator);
-//
-//	imwrite("output.jpg", outputImage);
-//	namedWindow("Input Image");
-//	namedWindow("Output Image");
-//
-//	imshow("Input Image", inputImage);
-//	imshow("Output Image", outputImage);
-//	waitKey(0);
-//
-//	return 0;
-//}
+int main(int argc, char *argv[])
+{
+	Mat inputImage = imread(argv[3],CV_LOAD_IMAGE_COLOR);
 
-void main() {
-	Mat inputImage = imread("lena.jpg", CV_LOAD_IMAGE_COLOR);
-	Mat outputImage(inputImage.rows, inputImage.cols, CV_8UC3);
+	// Kiểm tra xem có mở được ảnh hay không
+	if (inputImage.data == NULL) {
+		cout << "Khong the mo duoc anh" << endl;
+		return 0;
+	}
+	Mat outputImage;
+
+	PixelInterpolate *interpolator;
+
+	// Kiểm tra pp gán giá trị màu
+	if(strcmp(argv[2],"--mn") == 0)
+		interpolator = new NearestNeighborInterpolate;
+	else if(strcmp(argv[2],"--bl") == 0)
+		interpolator = new BilinearInterpolate;
+	else {
+		cout << strcmp(argv[2],"--bl")  << endl;
+		cout << "Interpolation error " << endl;
+		return 0;
+	}
+
 	GeometricTransformer gt;
-	
-	PixelInterpolate *pi = new NearestNeighborInterpolate();
+	if (strcmp(argv[1],"--zoom") == 0) {
+		// Lấy 2 đối số
+		gt.Scale(inputImage, outputImage, atof(argv[4]), atof(argv[5]), interpolator);
+	}
+	else if (strcmp(argv[1],"--rotate") == 0) {
+		gt.RotateKeepImage(inputImage, outputImage, atof(argv[4]), interpolator);
+	}
+	else if (strcmp(argv[1],"--rotateN") == 0) {
+		gt.RotateUnkeepImage(inputImage, outputImage, atof(argv[4]), interpolator);
+	} 
+	else{
+		cout << "Command error" << endl;
+		return 0;
+	}
 
+	// Xóa biến interpolator
+	delete interpolator;
 
+	imwrite("output.jpg", outputImage);
+	namedWindow("Input Image");
+	namedWindow("Output Image");
 
-	gt.RotateUnkeepImage(inputImage, outputImage,0, pi);
-	delete pi;
-	
-	imshow("input", inputImage);
-	imshow("output", outputImage);
+	imshow("Input Image", inputImage);
+	imshow("Output Image", outputImage);
 	waitKey(0);
+
+	return 0;
 }
